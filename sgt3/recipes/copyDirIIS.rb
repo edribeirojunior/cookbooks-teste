1581ff4edff6b57ae6a40f1f2bd74252367300da
Edson Ribeiro Junior <edribeirojunior@gmail.com>
2015-09-30 15:36:44 -0300

    scripts e alteracoes de funcionamento da recipe sgt3

--- sgt3/recipes/copyDirIIS.rb
+++ sgt3/recipes/copyDirIIS.rb
@@ -5,7 +5,7 @@ end
 
 node['SGT']['CONGLOMERADOS'].each do |lyr|
 	execute 'Copy IIS files' do
-	  command "robocopy C:\\Temp\\sgt3-IIS\\Navita.NGN.WEB C:\\Aplicativos_Navita\\#{lyr[1]} /s COPY:DT"
+	  command "robocopy C:\\Temp\\sgt3-IIS\\Navita.NGN.WEB C:\\Aplicativos_Navita\\#{lyr[1]} /s /COPY:DT"
 	  returns [0, 1]
 	end
 end
