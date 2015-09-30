4c9b77015f6aa651a44463bf2436c3434b0f5bd4
root <root@NVTCHEF01.nvta.corp>
2015-09-29 21:16:16 -0300

    New connection string path

--- sgt3/recipes/downloadNextractIIS.rb
+++ sgt3/recipes/downloadNextractIIS.rb
@@ -1,6 +1,12 @@
   bag = data_bag_item("sgt3", "files")
   target = "c:/Temp/" + bag["IISFILE"]
 
+ directory 'C:\\Temp' do
+    action :create
+    recursive true
+ end
+
+
   remote_file "#{target}" do
     source bag["FTP_PATH"] + bag["IISFILE"]
     action :create
---
+++ sgt3/recipes/startServiceApp.rb
@@ -0,0 +1,3 @@
+service "Navita Hosting - NGN" do
+  action :start
+end
