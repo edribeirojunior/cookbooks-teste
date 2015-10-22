c4c66b1772ba4d51a694c2c118d4847e87f6b302
Edson Ribeiro Junior <edribeirojunior@gmail.com>
2015-10-22 00:08:13 -0200

    ajuste exec, particionando arquivos em relacao a servicos

--- exec.sh
+++ exec.sh
@@ -9,22 +9,6 @@ fi
 
 echo $chef_resource
 
-###Ambientes UOL CLIENTES
-##CACIQUE-SGT-IISAPP-01 - 172.16.15.9
-./execRecipeEN.sh CACIQUE-SGT-IISAPP-01.nvta.corp $chef_resource > /var/log/chef/172.16.15.9.txt &
-#DAMOVO-SGT-IISAPP-01 - 172.16.15.10
-./execRecipeEN.sh DAMOVO.nvta.corp $chef_resource > /var/log/chef/172.16.15.10.txt &
-##ELDORADO-SGT-IISAPP-01 - 172.16.15.11
-./execRecipeEN.sh ELDORADO.nvta.corp $chef_resource > /var/log/chef/172.16.15.11.txt &
-##HOTBILL-SGT-IISAPP-01 - 172.16.15.13
-./execRecipeEN.sh HOTBILL.nvta.corp $chef_resource > /var/log/chef/172.16.15.13.txt &
-##TNX-SGT-IISAPP-01 - 172.16.15.14
-./execRecipeEN.sh TNX.nvta.corp $chef_resource > /var/log/chef/172.16.15.14.txt &
-##DAMOVO-SGT-IISAPP-02 - 172.16.15.15
-#./execRecipePT.sh DAMOVO-SGT-IISAPP-02.nvta.corp $chef_resource > /var/log/chef/172.16.15.15.txt &
-##JOHNSON-SGT-IISAPP-01 - 172.16.15.19
-./execRecipeEN.sh JOHNSON-SGT-IIS-01.nvta.corp $chef_resource > /var/log/chef/172.16.15.19.txt &
-
 ##SGT-DEV - 172.31.20.54
 #./execRecipeEN.sh SGT-dev.nvta.corp $chef_resource > /var/log/chef/172.31.20.54.txt &
 
