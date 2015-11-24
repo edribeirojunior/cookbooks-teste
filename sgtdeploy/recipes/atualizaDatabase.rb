81a87a207ecf3bf986f510c749a9c4ef143daaea
Edson Ribeiro Junior <edribeirojunior@gmail.com>
2015-11-24 11:35:08 -0200

    ajustos e retornos

--- sgtdeploy/recipes/atualizaDatabase.rb
+++ sgtdeploy/recipes/atualizaDatabase.rb
@@ -1,5 +1,5 @@
 bag = data_bag_item("sgtdeploy", "files")
-orig = "C:\\Temp\\" + bag["VERSION"] + "\\IIS\\GTC\\AtualizaBanco\\Scripts"
+orig = "C:\\Temp\\" + bag["VERSION"] + "\\IIS\\GTC\\AtualizaBanco\\"
 iis_gtc = node["SGT"]["IIS_GTC"]
 dest = iis_gtc + "\\AtualizaBanco\\Scripts"
 error_log = iis_gtc + "\\AtualizaBanco\\erro_Banco.txt"
