be4e965b29fbe8b63c9d28d6a9d479d9f303d42f
Edson Ribeiro Junior <edribeirojunior@gmail.com>
2015-12-07 17:27:08 -0200

    setconfig test

--- navita-connect/recipes/setWebConfigCas.rb
+++ navita-connect/recipes/setWebConfigCas.rb
@@ -1,9 +1,11 @@
+LINK_CONNECT= node['CONNECT']['LINK_CONNECT']
 RDS=node['CONNECT']['DB']['RDS']
 USER_DATABASE=node['CONNECT']['DB']['USER_DATABASE']
 PASSWD_DATABASE=node['CONNECT']['DB']['PASSWD_DATABASE']
 template "/opt/cas/webapps/cas/WEB-INF/cas.properties" do
   source "cas.properties.erb"
   variables = {
+                :link_connect => LINK_CONNECT,
                 :rds => RDS ,
                 :user_database => USER_DATABASE ,
                 :passwd_database => PASSWD_DATABASE ,
--- navita-connect/recipes/setWebConfigPortal.rb
+++ navita-connect/recipes/setWebConfigPortal.rb
@@ -1,7 +1,13 @@
 LINK_CONNECT= node['CONNECT']['LINK_CONNECT']
+RDS=node['CONNECT']['DB']['RDS']
+USER_DATABASE=node['CONNECT']['DB']['USER_DATABASE']
+PASSWD_DATABASE=node['CONNECT']['DB']['PASSWD_DATABASE']
   template "/opt/portal/portal-start.sh" do
     source "portal-start.sh.erb"
     variables = {
-                  :link_connect => LINK_CONNECT
+                  :link_connect => LINK_CONNECT ,
+                  :rds => RDS ,
+                  :user_database => USER_DATABASE ,
+                  :passwd_database => PASSWD_DATABASE ,
               }
   end
