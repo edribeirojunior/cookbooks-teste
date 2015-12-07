d564b6197b4ed2dd85d8f62b9697d7f66adcfb76
Edson Ribeiro Junior <edribeirojunior@gmail.com>
2015-12-07 12:17:48 -0200

    Ajuste configs ruby

--- navita-connect/recipes/setWebConfigCas.rb
+++ navita-connect/recipes/setWebConfigCas.rb
@@ -3,9 +3,9 @@ USER_DATABASE=node['CONNECT']['DB']['USER_DATABASE']
 PASSWD_DATABASE=node['CONNECT']['DB']['PASSWD_DATABASE']
 template "/opt/cas/webapps/cas/WEB-INF/cas.properties" do
   source "cas.properties.erb"
-  variables({
-                :rds => RDS
-                :user_database => USER_DATABASE
-                :passwd_database => PASSWD_DATABASE
-  })
+  variables = {
+                :rds => RDS ,
+                :user_database => USER_DATABASE ,
+                :passwd_database => PASSWD_DATABASE ,
+  }
 end
--- navita-connect/recipes/setWebConfigReport.rb
+++ navita-connect/recipes/setWebConfigReport.rb
@@ -3,9 +3,9 @@ USER_DATABASE=node['CONNECT']['DB']['USER_DATABASE']
 PASSWD_DATABASE=node['CONNECT']['DB']['PASSWD_DATABASE']
 template "/opt/report/report-start.sh" do
   source "report-start.sh.erb"
-  variables({
+  variables{
                 :rds => RDS
                 :user_database => USER_DATABASE
                 :passwd_database => PASSWD_DATABASE
-            })
+            }
 end
