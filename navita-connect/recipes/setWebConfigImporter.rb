8080567b2bb164f87c21d8d3efcda752e9afcdfc
Edson Ribeiro Junior <edribeirojunior@gmail.com>
2015-12-07 17:38:14 -0200

    setconfig httpd

--- navita-connect/recipes/setWebConfigImporter.rb
+++ navita-connect/recipes/setWebConfigImporter.rb
@@ -1,6 +1,6 @@
-RDS=node['CONNECT']['DB']['RDS']
-USER_DATABASE=node['CONNECT']['DB']['USER_DATABASE']
-PASSWD_DATABASE=node['CONNECT']['DB']['PASSWD_DATABASE']
+RDS = node['CONNECT']['DB']['RDS']
+USER_DATABASE = node['CONNECT']['DB']['USER_DATABASE']
+PASSWD_DATABASE = node['CONNECT']['DB']['PASSWD_DATABASE']
 template "/opt/importer/importer-start.sh" do
   source "importer-start.sh.erb"
   variables = {
