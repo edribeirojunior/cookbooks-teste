e379d3c480a894c607d856837dd0cab2a104dc22
edribeirojunior <edribeirojunior@gmail.com>
2016-06-10 20:06:19 -0300

    ajustes files create and json

--- microdata/recipes/download_and_extract.rb
+++ microdata/recipes/download_and_extract.rb
@@ -1,6 +1,3 @@
-
-depends 'sevenzip'
-
 node['MICRODATA']['PROD'].each do |lyr|
 
   directory 'C:\\Temp' do
@@ -11,6 +8,17 @@ node['MICRODATA']['PROD'].each do |lyr|
   seven_zip_archive "#{node['MICRODATA']['PROD']}#{lyr['name']}" do
     path      "#{node['MICRODATA']['TEMPATH']}#{lyr['name']}"
     source    "#{node['MICRODATA']['TEMPATH']}#{lyr['name']}#{lyr['version']}"
+    overwrite true
+    action :extract
+  end
+
+end
+
+node['Service']['PROD'].each do |service|
+
+  seven_zip_archive "#{node['Service']['PROD']}#{service['name']}" do
+    path      "#{node['MICRODATA']['TEMPATH']}#{service['name']}"
+    source    "#{node['MICRODATA']['TEMPATH']}#{service['name']}#{service['version']}"
     overwrite true
     action :extract
   end
