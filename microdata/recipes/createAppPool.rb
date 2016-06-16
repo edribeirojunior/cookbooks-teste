95afa317da0d184f13d41eda93d1cdf8396eab3f
edribeirojunior <edribeirojunior@gmail.com>
2016-06-16 15:51:40 -0300

    add options iis_pool

--- microdata/recipes/createAppPool.rb
+++ microdata/recipes/createAppPool.rb
@@ -7,7 +7,12 @@ node['MICRODATA']['PROD'].each do |lyr|
 	# Cria pool de acrodo com prod
 	iis_pool "#{lyr['name']}" do
 	  runtime_version "4.0"
-	  pipeline_mode :Integrated
+		thirty_two_bit true
+		pool_identity :LocalSystem
+		idle_timeout '00:00:00'
+		pipeline_mode :Integrated
+		disallow_rotation_on_config_change true
+		disallow_overlapping_rotation true
 	  action :add
 	end
 end
